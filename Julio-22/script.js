3d9c8ae81599622124953fc2fe9d4bb3069b1696
NestorRiberoPUJ1 <67485144+NestorRiberoPUJ1@users.noreply.github.com>
2024-07-22 13:00:48 -0500

    Create script.js

---
+++ Julio-22/script.js
@@ -0,0 +1,33 @@
+
+
+/* VARIABLES */
+
+//LOCALES
+let nombre = "Julio";
+//GLOBALES
+var apellido = "Cesar";
+//CONSTANTES
+const PI = 3.1416;
+
+/* TIPOS DE DATOS */
+let numero = 10;
+let decimal = 10.5;
+let caracter= 'a';
+let texto = "Hola Mundo";
+let booleano = true;
+let nulo = null;
+let indefinido = undefined;
+
+/* ESTRUCTURA DE DATOS */
+let arreglo = ["Julio", "Cesar", "Garcia"];
+let objeto = {
+    nombre: "Julio",
+    edad: 22,
+    nacionalidad: "Mexicana"
+};
+
+/* ACCEDER A VALORE DE ESTRUCTURAS */
+console.log(arreglo[2]);
+console.log(objeto.nombre);
+console.log(objeto["nacionalidad"]);
+
